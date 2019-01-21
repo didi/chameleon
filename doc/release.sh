a8539306c17c22f57bf325145e6fcad988de7e8b
Conan <zhangnanconan@didichuxing.com>
2019-01-21 14:33:17 +0800

    docs update

--- doc/release.sh
+++ doc/release.sh
@@ -1,12 +1,13 @@
 GIT_SITE="git@github.com"
-TEAM_NAME="didi"
-PROJECT_NAME="chameleon"
+TEAM_NAME="beatles-chameleon"
+PROJECT_NAME="${TEAM_NAME}.github.io"
 
 set -e
 
-# mac下如果使用自带的sed命令需要修改为以下
-sed -i 's#/doc/#/chameleon/doc/#g' "book.json"
 
+
+# mac下如果使用自带的sed命令需要修改为以下
+# sed -i '' "s#chameleon/#doc/#g" "book.json"
 if [ ! -d "./${PROJECT_NAME}" ];then
 git clone ${GIT_SITE}:${TEAM_NAME}/${PROJECT_NAME}.git  --depth=1
 else
@@ -25,11 +26,9 @@ gitbook install && gitbook build && rm -rf ./${PROJECT_NAME}/doc && mv -f ./_boo
 rm -rf ./${PROJECT_NAME}/doc/website
 
 # mac下如果使用自带的sed命令需要修改为以下
-sed -i 's#/chameleon/doc/#/doc/#g' "book.json"
-
+# sed -i '' 's#doc/#chameleon/#g' "book.json"
 cd ./${PROJECT_NAME}
-git checkout -b gh-pages
 git add .
-git commit -am "help document"
-# git push origin gh-pages
-# cd ../
+git commit -am "release book"
+git push
+cd ../
