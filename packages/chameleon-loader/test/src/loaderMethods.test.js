93fd6ad8e468036ba0bda7d76d599a16ea03e78c
yangyiliang <yangyiliang@didichuxing.com>
2019-01-25 15:42:02 +0800

    chameleon-loader prepareParseUsingComponents

---
+++ packages/chameleon-loader/test/src/loaderMethods.test.js
@@ -0,0 +1,58 @@
+const loaderMethods = require('../../src/loaderMethods.js');
+const expect = require('chai').expect;
+const path = require('path');
+
+describe('prepareParseUsingComponents', function(){
+  it('/npm/', function() {
+    let originObj = {
+      scroller: '/npm/cml-ui/scroller/scroller'
+    }
+    let loaderContext = {
+      resourcePath: path.join(__dirname, './project/src/pages/page1.cml')
+    }
+
+    let context = path.join(__dirname, './project');
+    let result = loaderMethods.prepareParseUsingComponents({
+      loaderContext,
+      context,
+      originObj,
+      cmlType: 'wx'
+    });
+
+    let expectPath = path.join(__dirname, './project/node_modules/cml-ui/scroller/scroller.cml')
+    console.log(result)
+    result.forEach(item=>{
+      if (item.tagName === 'scroller') {
+        expect(item.filePath).to.be.equal(expectPath);
+        expect(item.isNative).to.be.equal(false);
+      }
+    })
+  })
+
+  it('../npm/', function() {
+    let originObj = {
+      scroller: './../npm/cml-ui/scroller/scroller'
+    }
+    let loaderContext = {
+      resourcePath: path.join(__dirname, './project/src/pages/page1.cml')
+    }
+
+    let context = path.join(__dirname, './project');
+    let result = loaderMethods.prepareParseUsingComponents({
+      loaderContext,
+      context,
+      originObj,
+      cmlType: 'wx'
+    });
+
+    let expectPath = path.join(__dirname, './project/node_modules/cml-ui/scroller/scroller.cml')
+    result.forEach(item=>{
+      if (item.tagName === 'scroller') {
+        expect(item.filePath).to.be.equal(expectPath);
+        expect(item.isNative).to.be.equal(false);
+      }
+    })
+  })
+})
+
+
