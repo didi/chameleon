18fc1c5d67ef3e48f90d1b35ab61d52e54805cba
JiM-W <550116990@qq.com>
2019-01-28 19:33:27 +0800

    支付宝样式支持

--- packages/chameleon-template-parse/src/compile-template-vue.js
+++ packages/chameleon-template-parse/src/compile-template-vue.js
@@ -24,6 +24,7 @@ exports.compileTemplateForVue = function (source, type, options) {
   source = processTemplate.postParseLtGt(source);
   // 预处理c-animation 标签，给这个标签增加一个 c-bind:transitionend = "_animationCb(value,$event)"，注意这个必须在所有预处理的最后，因为前面的预处理兼容了jsx的语法；
   source = processTemplate.preParseAnimation(source, type);
+  source = processTemplate.alipayComponentsWraped(source, type, options);
 
   if (type === 'web') {
     source = compileWebTemplate(source, type, options).code;
--- packages/chameleon-template-parse/test/index.js
+++ packages/chameleon-template-parse/test/index.js
@@ -1,12 +1,13 @@
 const compileTemplate = require('../src/index.js');
 const source = `<template>
-<view c-animation="{{animationData}}"></view>
+<button></button>
+<thirdComp1 :style="computedStyle" :class="cls"></thirdComp1>
 </template>`
 // <view><text :class="{{true? 'bg-green':''}}" >fafafa</text></view>
 // <view><text :class="true? 'bg-green':''" >fafafa</text></view>
 //
 // let result = compileTemplate(source,'web');
-let options = {lang: 'cml',
+let options = {lang: 'vue',
   buildInComponents: {button: "cml-buildin-button"},
   cmss: {
     rem: true,
@@ -20,12 +21,26 @@ let options = {lang: 'cml',
     autoprefixOptions: {
       browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
     }
-  } };
+  },
+  usingComponents: [{
+    tagName: 'thirdComp1',
+    refUrl: '/path/to/ref1',
+    filePath: 'path/to/real1',
+    isNative: true
+  }, {
+    tagName: 'thirdComp2',
+    refUrl: '/path/to/ref2',
+    filePath: 'path/to/real2',
+    isNative: false
+  }]
+};
 console.log('before-compile', source);
+let result_web = compileTemplate(source, 'web', options);
 let result_wx = compileTemplate(source, 'wx', options);
 let result_baidu = compileTemplate(source, 'wx', options);
 let result_alipay = compileTemplate(source, 'alipay', options);
 // let result_alipay = compileTemplate(source,'alipay',{lang:'cml'});
-console.log('result_wx', result_wx)
+// console.log('result_web', result_web)
+// console.log('result_wx', result_wx)
 console.log('result_alipay', result_alipay)
-console.log('result_baidu', result_baidu)
+// console.log('result_baidu', result_baidu)
