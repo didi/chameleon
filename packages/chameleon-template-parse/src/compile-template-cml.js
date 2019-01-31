373ad0d2dcbbd6294ab47ba9303196013e189300
JiM-W <550116990@qq.com>
2019-01-31 15:14:18 +0800

    完成支付宝模板解析-这种解决方案仍待商榷

--- packages/chameleon-template-parse/src/compile-template-cml.js
+++ packages/chameleon-template-parse/src/compile-template-cml.js
@@ -13,8 +13,6 @@ exports.compileTemplateForCml = function (source, type, options) {
   source = processTemplate.preDisappearAnnotation(source);
   // 预处理：<   >  ==>  _cml_lt_lmc_  _cml_gt_lmc_,这么做的目的为了防止 preParseMustache 解析 > < 中间的内容报错，所以需要将 > < 先转化 gt lt的形式，等 preParseMustache 解析完毕之后即可将其转化回来；
   source = processTemplate.preParseGtLt(source);
-  debugger;
-  source = processTemplate.preParseAliComponent(source, type, options);
   source = processTemplate.preParseDiffPlatformTag(source, type);
   // 预处理:属性 jsx不支持 :name="sth" ==> v-bind:name="sth"
   source = processTemplate.preParseBindAttr(source);
@@ -26,6 +24,8 @@ exports.compileTemplateForCml = function (source, type, options) {
   source = processTemplate.postParseLtGt(source);
   // 预处理c-animation 标签，给这个标签增加一个 c-bind:transitionend = "_animationCb(value,$event)",注意这个必须在所有预处理的最后，因为前面的预处理兼容了jsx的语法；
   source = processTemplate.preParseAnimation(source, type);
+  // source = processTemplate.preParseAliComponent(source, type, options);
+  source = processTemplate.alipayComponentsWraped(source, type, options);
   debugger;
   debugger;
   if (type === 'web') {
