ba3c84cf3b7df4dda15f20e884cec87ee7ebd75f
quyatong <quyatong@didichuxing.com>
2019-02-18 17:54:01 +0800

    增加配置重置

--- packages/chameleon-linter/config/built-in-components/index.js
+++ packages/chameleon-linter/config/built-in-components/index.js
@@ -34,13 +34,17 @@ function isPackageUpdated() {
     _builtinCompsInfo.name = pakcageInfo.name;
     _builtinCompsInfo.version = pakcageInfo.version;
   }
+  else {
+    _builtinCompsInfo = {
+      name: '', version: '', components: {}
+    };
+  }
   return true;
 }
 
 function getBuiltinComponents() {
   let result = {};
   let inDir = config.getCurrentWorkspace() + '/node_modules/chameleon-ui-builtin/components';
-  
   if (fs.existsSync(inDir)) {
     Parser.flatEntrance(inDir).forEach(filterFile => {
       let content = parser.resetPath(filterFile).getJsonResultsWithComponentName();
@@ -48,7 +52,7 @@ function getBuiltinComponents() {
     });
   }
   _builtinCompsInfo.components = result;
-  return result; 
+  return result;
 }
 
 function getStoredComponentInfo() {
