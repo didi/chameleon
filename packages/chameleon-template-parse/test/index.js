38a372b87e030e666ff6626a4b23dbc515142ec6
JiM-W <550116990@qq.com>
2019-02-21 16:08:04 +0800

    多态组件不进行语法校验,仅仅跨端组件进行语法校验

--- packages/chameleon-template-parse/test/index.js
+++ packages/chameleon-template-parse/test/index.js
@@ -1,14 +1,34 @@
 const compileTemplate = require('../src/index.js');
-const source = `<scroller height="{{-1}}">
-<view v-if="false">  v-if</view>
-<view v-else-if>  v-if</view>
-</scroller> `
+const source = `<scroller :height="-1">
+<view> <text>cpxtest</text> </view>
+<view :style="'width:'+cpx+'cpx;'+'height:'+cpx2+'cpx;background-color:red'">cpx</view>
+<view @click="handleClick">change-component-vue</view>
+<view @click="handleClick(1,2,3)">change-component</view>
+  <component :is="currentComp" :image-src="chameleonSrc" title="this is title"></component>
+  <show v-show="show"></show>
+  <view v-text="message"></view>
+  <view :class="5 > 6 ? 'cls1':'cls2'" :style="5 < 6 ? 'width:300cpx':'height:300cpx'">
+    {{5 > 6?'5>6' : '5 < 6'}}</view>
+  <view v-if="false">c-if</view>
+  <view v-else-if="true">c-else-if</view>
+  <view v-else>c-else</view>
+  
+  <demo-com :image-src="chameleonSrc" title="标题"></demo-com>
+  <view :style="computedStyle">computedStyle</view>
+  <view><text>v-model的使用</text></view>
+  <input type="text" v-model=" modelValueTest  "/>
+  <text>{{modelValueTest}}</text>
+
+  <comp v-model=" modelValueTest2 " ></comp>
+  <view><text>组件使其改变{{modelValueTest2}}</text></view>
+
+</scroller>`
 // <view><text :class="{{true? 'bg-green':''}}" >fafafa</text></view>
 // <view><text :class="true? 'bg-green':''" >fafafa</text></view>
 //
 // let result = compileTemplate(source,'web');
-let options = {lang: 'cml',
-  filePath: '/Users/didi/components',
+let options = {lang: 'vue',
+  filePath: '/Users/didi/components.cml',
   buildInComponents: {button: "cml-buildin-button"},
   cmss: {
     rem: true,
@@ -36,12 +56,12 @@ let options = {lang: 'cml',
   }]
 };
 console.log('before-compile', source);
-let result_web = compileTemplate(source, 'web', options);
+// let result_web = compileTemplate(source, 'web', options);
 let result_wx = compileTemplate(source, 'wx', options);
 // let result_baidu = compileTemplate(source, 'wx', options);
 // let result_alipay = compileTemplate(source, 'alipay', options);
 // let result_alipay = compileTemplate(source,'alipay',{lang:'cml'});
-console.log('result_web', result_web)
-console.log('result_wx', result_wx)
+// console.log('result_web', result_web)
+// console.log('result_wx', result_wx)
 // console.log('result_alipay', result_alipay)
 // console.log('result_baidu', result_baidu)
