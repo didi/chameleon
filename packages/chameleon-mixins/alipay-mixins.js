a6333fcf189f0a5d42a466384c3be51b51f6f6b5
JiM-W <550116990@qq.com>
2019-02-22 11:06:39 +0800

    alipay c-model

--- packages/chameleon-mixins/alipay-mixins.js
+++ packages/chameleon-mixins/alipay-mixins.js
@@ -11,11 +11,12 @@ commonMixins.merge(_.mixins.methods, {
   [_.eventEmitName]: function(eventKey, detail) {
     console.log('alipay-props', this.props, detail)
     let eventKeyProps = this.props["data-event" + eventKey];
+    let alipayDetail = detail ;
     if (Object.prototype.toString.call(detail) === '[object Object]' && this.props['data-modelkey']) {
       let aliModelKey = this.props['data-modelkey'] || '';
-      detail = Object.assign(detail, {aliModelKey})
+      alipayDetail = Object.assign(detail, {aliModelKey})
     }
-    eventKeyProps && this.props[_.cmlPropsEventProxy.key](eventKeyProps, detail);
+    eventKeyProps && this.props[_.cmlPropsEventProxy.key](eventKeyProps, alipayDetail);
     if (this.$__checkCmlEmit__) {
       this.$__checkCmlEmit__(eventKey, detail);
     }
