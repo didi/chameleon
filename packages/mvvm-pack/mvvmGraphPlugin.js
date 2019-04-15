280d2f0b084f6626148298c2953096c53ba7afd0
yylgit <394720943@qq.com>
2019-04-15 21:22:21 +0800

    mvvm

--- packages/mvvm-pack/mvvmGraphPlugin.js
+++ packages/mvvm-pack/mvvmGraphPlugin.js
@@ -5,18 +5,21 @@ const MvvmCompiler = require('./compiler.js');
 class mvvmGraphPlugin {
   constructor(options = {}) {
     this.options = options;
-    
   }
   apply(compiler) {
+    let self = this;
     let npmName = cml.config.get().extPlatform[this.options.cmlType];
     let PlatformPlugin = require(path.join(cml.projectRoot, 'node_modules', npmName)); // eslint-disable-line
     let plugin = new PlatformPlugin(this.options);
     let mvvmCompiler = new MvvmCompiler(compiler);
-    cml.event.on('find-component', function( {context, cmlFilePath, comPath, cmlType}, result) {
-
+    // 监听cml中查找组件
+    cml.event.on('find-component', function({context, cmlFilePath, comPath, cmlType}, result) {
+      // 如果是当前端 则触发用户的查找事件
+      if (cmlType === self.options.cmlType) {
+        mvvmCompiler.emit('find-component', {context, cmlFilePath, comPath, cmlType}, result);
+      }
     })
     plugin.register(mvvmCompiler);
-    
     compiler.plugin('should-emit', function(compilation) {
       mvvmCompiler.run(compilation.modules);
       // 返回false 不进入emit阶段
