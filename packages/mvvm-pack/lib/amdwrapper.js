e76a3c133fa6092d7d7f9568f3e95108d5dc93d8
yylgit <394720943@qq.com>
2019-04-20 17:02:01 +0800

    amd

---
+++ packages/mvvm-pack/lib/amdwrapper.js
@@ -0,0 +1,8 @@
+
+module.exports = function(content, modId) {
+  if (!/^\s*cmldefine\s*\(/.test(content)) {
+    content = 'cmldefine(\'' + modId + '\', function(require, exports, module){ ' + content +
+        ' \r\n});';
+  }
+  return content;
+}
