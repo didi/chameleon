7ed6f4b3a4fb3e3ecd691392b1ec95239347f707
JiM-W <550116990@qq.com>
2019-05-05 11:07:54 +0800

    删除无效更改

--- packages/mvvm-template-parser/lib/test-index.js
+++
@@ -1,8 +0,0 @@
-const {vueToCml} = require('./process-template.js');
-let source = `<view class="demo-com">
-<view :id="value" v-bind:name="name">index-handleTouchStart</view>
-<view @click="handleClick" v-on:click="handleClick" >index-handletap</view>
-<view @click.stop="handleClick" v-on:click.stop="handleClick">index-handletap</view>
-</view>`
-const result = vueToCml(source);
-console.log('vuetocml',source)
