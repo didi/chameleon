e50b97ba3c6c6d328abbf8c79f5ce22bc3921ae7
yylgit <394720943@qq.com>
2019-05-29 10:27:33 +0800

    delete debuger

--- packages/mvvm-pack/lib/amdbootstrap.module.js
+++ packages/mvvm-pack/lib/amdbootstrap.module.js
@@ -31,7 +31,6 @@
     var ret = (typeof factory == 'function')
       ? factory.apply(mod, [cmlrequire, mod.exports, mod])
       : factory;
-    debugger
     if (ret) {
       mod.exports = ret;
     }
