3d8266b7597d595c63790919f7a5f533067ff6a0
kevinluohuan <kevinluohuan@didichuxing.com>
2019-06-11 20:16:57 +0800

    Chameleon-linter 对模板和接口中全局变量校验问题修改

--- packages/chameleon-linter/checkers/script.js
+++ packages/chameleon-linter/checkers/script.js
@@ -1,175 +1,6 @@
 const traverse = require('@babel/traverse')['default'];
 const deepTraverse = require('traverse');
-const uniq = require('lodash.uniq');
-const config = require('../config');
-
-const DEFAULT_TOKENS_MAP = {
-  WEEX: ['weex', 'global'],
-  WX: ['wx', 'global'],
-  BAIDU: ['swan', 'global'],
-  ALIPAY: ['my', 'global'],
-  WEB: [
-    'postMessage', 'blur', 'focus', 'close', 'frames', 'self',
-    'window', 'parent', 'opener', 'top', 'length', 'closed',
-    'location', 'document', 'origin', 'name', 'history',
-    'locationbar', 'menubar', 'personalbar', 'scrollbars',
-    'statusbar', 'toolbar', 'status', 'frameElement', 'navigator',
-    'customElements', 'external', 'screen', 'innerWidth',
-    'innerHeight', 'scrollX', 'pageXOffset', 'scrollY',
-    'pageYOffset', 'screenX', 'screenY', 'outerWidth', 'outerHeight',
-    'devicePixelRatio', 'clientInformation', 'screenLeft',
-    'screenTop', 'defaultStatus', 'defaultstatus', 'styleMedia',
-    'onanimationend', 'onanimationiteration', 'onanimationstart',
-    'onsearch', 'ontransitionend', 'onwebkitanimationend',
-    'onwebkitanimationiteration', 'onwebkitanimationstart',
-    'onwebkittransitionend', 'isSecureContext', 'onabort', 'onblur',
-    'oncancel', 'oncanplay', 'oncanplaythrough', 'onchange', 'onclick',
-    'onclose', 'oncontextmenu', 'oncuechange', 'ondblclick', 'ondrag',
-    'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart',
-    'ondrop', 'ondurationchange', 'onemptied', 'onended', 'onerror',
-    'onfocus', 'oninput', 'oninvalid', 'onkeydown', 'onkeypress',
-    'onkeyup', 'onload', 'onloadeddata', 'onloadedmetadata',
-    'onloadstart', 'onmousedown', 'onmouseenter', 'onmouseleave',
-    'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel',
-    'onpause', 'onplay', 'onplaying', 'onprogress', 'onratechange', 'onreset',
-    'onresize', 'onscroll', 'onseeked', 'onseeking', 'onselect', 'onstalled',
-    'onsubmit', 'onsuspend', 'ontimeupdate', 'ontoggle', 'onvolumechange',
-    'onwaiting', 'onwheel', 'onauxclick', 'ongotpointercapture',
-    'onlostpointercapture', 'onpointerdown', 'onpointermove',
-    'onpointerup', 'onpointercancel', 'onpointerover', 'onpointerout',
-    'onpointerenter', 'onpointerleave', 'onafterprint', 'onbeforeprint',
-    'onbeforeunload', 'onhashchange', 'onlanguagechange', 'onmessage',
-    'onmessageerror', 'onoffline', 'ononline', 'onpagehide', 'onpageshow',
-    'onpopstate', 'onrejectionhandled', 'onstorage', 'onunhandledrejection',
-    'onunload', 'performance', 'stop', 'open', 'alert', 'confirm', 'prompt',
-    'print', 'requestAnimationFrame', 'cancelAnimationFrame', 'requestIdleCallback',
-    'cancelIdleCallback', 'captureEvents', 'releaseEvents', 'getComputedStyle',
-    'matchMedia', 'moveTo', 'moveBy', 'resizeTo', 'resizeBy', 'getSelection', 'find',
-    'webkitRequestAnimationFrame', 'webkitCancelAnimationFrame', 'fetch',
-    'btoa', 'atob', 'createImageBitmap', 'scroll', 'scrollTo', 'scrollBy',
-    'onappinstalled', 'onbeforeinstallprompt', 'crypto', 'ondevicemotion',
-    'ondeviceorientation', 'ondeviceorientationabsolute', 'indexedDB',
-    'webkitStorageInfo', 'sessionStorage', 'localStorage', 'chrome',
-    'visualViewport', 'speechSynthesis', 'webkitRequestFileSystem',
-    'webkitResolveLocalFileSystemURL', 'openDatabase', 'applicationCache',
-    'caches', 'whichAnimationEvent', 'animationendEvent', 'infinity',
-    'SETTING', 'AppView', 'ExtensionOptions', 'ExtensionView', 'WebView',
-    'iconPath', '_app', '_ZOOM_', 'Feed', 'md5', '$', 'jQuery', 'Search',
-    'windmill', 'Lethargy', 'alertTimeOut', 'supportApps', 'lethargyX',
-    'lethargyY', 'iView', 'onModuleResLoaded', 'iEditDelete', 'infinityDrag',
-    'i', 'array', 'TEMPORARY', 'PERSISTENT', 'addEventListener',
-    'removeEventListener', 'dispatchEvent'
-  ]
-};
-
-// 这个path是否有需要校验的token
-const needCheck = function (path, tokenList) {
-  let flag = false;
-  for (let i = 0; i < tokenList.length; i++) {
-    let token = tokenList[i];
-    // 判断一个token是否是一个变量
-    if (checkToken(path, token)) {
-      // 如果是对象中的key是需要忽略的
-      flag = token;
-      break;
-    }
-  }
-
-  return flag;
-};
-
-// 是变量，并且不是对象的key值
-function checkToken(path, token) {
-  if (path.isIdentifier({ name: token })) {
-    // 是对象的key值
-    //   const api = {
-    //     name: weex,
-    //     };
-    let isObjectKey = path.parent.type === 'ObjectProperty' && path.parentKey === 'key'
-    // 对象成员
-    /**
-     * var a = {
-     *  weex: 'a'
-     * }
-     * console.log(weex.a);
-     */
-    let isObjectMember = path.parent.type === 'MemberExpression' && path.parentKey === 'property'
-
-    return !(isObjectKey || isObjectMember);
-  }
-  else {
-    return false;
-  }
-}
-
-const checkGlobal = function (ast, type = 'ALL') {
-  let tokensMap = DEFAULT_TOKENS_MAP;
-  const TOKENS_MAP = tokensMap;
-  type = type.toUpperCase();
-
-  let tokenList = [];
-
-  if (type === 'ALL') {
-    // 都要校验
-    tokenList = TOKENS_MAP[type];
-  }
-  else {
-    Object.keys(TOKENS_MAP).forEach(key => {
-      // 把自身的和All的去掉，其他端的token放进去
-      if (key !== type && key !== 'ALL') {
-        tokenList = tokenList.concat(TOKENS_MAP[key]);
-      }
-    })
-    // 然后要把自身的全局变量去掉
-    for (let i = 0;i < tokenList.length;) {
-      if (~TOKENS_MAP[type].indexOf(tokenList[i])) {
-        tokenList.splice(i, 1);
-      }
-      else {
-        i++;
-      }
-    }
-  }
-  tokenList = uniq(tokenList);
-  const messages = [];
-
-  traverse(ast, {
-    enter: (path) => {
-      // path是一个上下文
-      // 需要校验的变量值
-
-      let token = needCheck(path, tokenList);
-
-      // 如果存在该token
-      if (token) {
-        let globalVariable = true;
-        // 当前作用域
-        let next = path.scope;
-
-        while (next) {
-          // 如果当前作用域存在该变量 就不是全局变量
-          if (next.hasOwnBinding(token)) {
-            globalVariable = false;
-            break;
-          }
-          next = next.parent;
-        }
-
-
-        if (globalVariable && path.parent.type != 'ObjectMethod' && path.parent.type != 'ClassMethod') {
-          messages.push({
-            line: path.node.loc.start.line,
-            column: path.node.loc.start.column,
-            token: token,
-            msg: 'global variable [' + token + '] should not used in this file'
-          });
-        }
-      }
-    }
-  });
-  return messages;
-}
-
+const utils = require('../utils');
 
 /**
  * 获取接口定义
@@ -178,13 +9,20 @@ const checkGlobal = function (ast, type = 'ALL') {
  * @return {Object}     分析结果
  */
 const getInterfaces = (ast) => {
-  let result = {};
+  let result = {
+    name: '',
+    properties: {}
+  };
   ast.program.body.forEach(function (node) {
     if (node.type == 'InterfaceDeclaration') {
       let interfaceName = node.id.name;
-      result[interfaceName] = {};
+      result.name = interfaceName;
+      result.loc = {
+        line: node.id.loc.start.line,
+        column: node.id.loc.start.column
+      };
       node.body.properties.map((property) => {
-        result[interfaceName][property.key.name] = {
+        result.properties[property.key.name] = {
           type: property.value.type.replace(/TypeAnnotation/g, ''),
           line: property.key.loc.start.line,
           column: property.key.loc.start.column
@@ -197,11 +35,16 @@ const getInterfaces = (ast) => {
 
 /**
  * 获取类定义
- *
- * @param  {Object} ast ast
- * @return {Object}     类定义
+ * @param   {Object} ast ast
+ * @param   {Object} isComp a flag indentify whether the ast is component or an interface portion
+ * @return  {Object}     类定义
  */
-const getClass = (ast) => {
+const getClass = (ast, isComp) => {
+  return isComp ? getCompClassDef(ast) : getInterfacePortionClassDef(ast);
+};
+
+
+function getCompClassDef(ast) {
   let classes = [];
 
   traverse(ast, {
@@ -221,15 +64,18 @@ const getClass = (ast) => {
           });
         }
 
-        // 参数
         path.node.body.body.forEach(define => {
           if (define.key.name == 'props') {
             define.value.properties.forEach(property => {
               clazz.properties.push(property.key.name);
             });
           }
-          else if (define.type == 'ClassMethod') {
-            clazz.methods.push(define.key.name);
+          else if (define.key.name == 'methods') {
+            define.value.properties.filter(property => {
+              return property.type === 'ObjectMethod';
+            }).forEach(property => {
+              clazz.methods.push(property.key.name);
+            });
           }
           else {
             deepTraverse(define)
@@ -256,6 +102,7 @@ const getClass = (ast) => {
                       });
                     }
                   });
+                  clazz.methods.push(event.event);
                   clazz.events.push(event);
                 }
               });
@@ -268,7 +115,46 @@ const getClass = (ast) => {
   });
 
   return classes;
-};
+}
+
+function getInterfacePortionClassDef(ast) {
+  let classes = [];
+
+  traverse(ast, {
+    enter(path) {
+      if (path.node.type == 'ClassDeclaration') {
+        let clazz = {
+          interfaces: [],
+          properties: [],
+          events: [],
+          methods: []
+        };
+
+        // 接口
+        if (path.node['implements']) {
+          path.node['implements'].forEach(implament => {
+            clazz.interfaces.push(implament.id.name);
+          });
+        }
+
+        // 参数
+        path.node.body.body.forEach(define => {
+          if (define.type == 'ClassProperty') {
+            clazz.properties.push(define.key.name);
+          }
+          else if (define.type == 'ClassMethod') {
+            clazz.methods.push(define.key.name);
+          }
+        });
+
+        classes.push(clazz);
+      }
+    }
+  });
+
+  return classes;
+}
+
 
 /**
  * 校验接口与脚本
@@ -277,76 +163,65 @@ const getClass = (ast) => {
  * @return {Array}                数组
  */
 const checkScript = async (result) => {
-  let script;
-  let platforms = config.getPlatforms();
-
-  ['script'].concat(platforms).forEach(item => {
-    if (result[item] && result[item].ast) {
-      script = result[item];
+  let validPlatforms = Object.keys(result)
+    .filter(platform => {
+      return platform && (!~['json', 'template', 'style', 'script'].indexOf(platform));
+    })
+    .filter(platform => {
+      return platform && (platform != 'interface');
+    });
+  // add a script type for multi-file components.
+  result['interface'] && validPlatforms.concat('script').forEach(platform => {
+    let script;
+    let isComp = (platform === 'script');
+    if (result[platform] && result[platform].ast) {
+      script = result[platform];
     }
-  });
-
-  if (!result['interface'] && script) {
-    let interfaceFile = script.file.replace(new RegExp('\\.(' + platforms.join('|') + ')\\.cml$', 'ig'), '.interface');
-    if (/\.interface$/.test(interfaceFile)) {
-      result['interface'] = {
-        messages: [{
-          msg: 'file: [' + interfaceFile + '] was not found!'
-        }],
-        file: interfaceFile
-      };
-    }
-  }
-
-  if (result['interface'] && result['interface'].ast && script && script.ast) {
-    const interfaceDefine = getInterfaces(result['interface'].ast);
-    const classDefines = getClass(script.ast);
-
-    classDefines.forEach(clazz => {
-      clazz.interfaces.forEach(interfaceName => {
-        let define = interfaceDefine[interfaceName];
-
-        for (let key of Object.keys(define)) {
-          if ((define[key] && define[key].type == 'Generic') && clazz.properties.indexOf(key) == -1) {
+    if (result['interface'] && result['interface'].ast && script && script.ast) {
+      const interfaceDefine = getInterfaces(result['interface'].ast);
+      const classDefines = getClass(script.ast, isComp);
+      classDefines.forEach(clazz => {
+        clazz.interfaces.forEach(interfaceName => {
+          let define = interfaceDefine.name === interfaceName ? interfaceDefine.properties : null;
+          if (!define) {
             result['interface'].messages.push({
-              line: define[key].line,
-              column: define[key].column,
-              token: key,
-              msg: 'property [' + key + '] is not found in file [' + script.file + ']'
+              msg: `The implement class name: "${interfaceName}" used in file: "${utils.toSrcPath(script.file)}" doesn\'t match the name defined in it\'s interface file: "${utils.toSrcPath(result['interface'].file)}"`
             });
+            return;
           }
-          else if ((define[key] && define[key].type == 'Function') && clazz.methods.indexOf(key) == -1) {
-            platforms.forEach(platform => {
-              if (result[platform]) {
-                result['interface'].messages.push({
-                  line: define[key].line,
-                  column: define[key].column,
-                  token: key,
-                  msg: 'method [' + key + '] is not found in file [' + script.file + ']'
-                });
-              }
-            });
+          for (let key of Object.keys(define)) {
+            if ((define[key] && define[key].type == 'Generic') && clazz.properties.indexOf(key) == -1) {
+              result['interface'].messages.push({
+                line: define[key].line,
+                column: define[key].column,
+                token: key,
+                msg: `interface property "${key}" is not defined for platform ${script.platform} in file "${utils.toSrcPath(script.file)}"`
+              });
+            }
+            else if ((define[key] && define[key].type == 'Function' && clazz.methods.indexOf(key) === -1)) {
+              result['interface'].messages.push({
+                line: define[key].line,
+                column: define[key].column,
+                token: key,
+                msg: `interface method "${key}" is not defined for platform ${script.platform} in file "${utils.toSrcPath(script.file)}"`
+              });
+            }
           }
-        }
 
-        clazz.events.forEach(event => {
-          if (!define[event.event] || (define[event.event] && (define[event.event].type != 'Function'))) {
-            script.messages.push({
-              line: event.line,
-              column: event.column,
-              token: event.event,
-              msg: 'event [' + event.event + '] is not defined in interface file [' + result['interface'].file + ']'
-            });
-          }
+          clazz.events.forEach(event => {
+            if (!define[event.event] || (define[event.event] && (define[event.event].type != 'Function'))) {
+              script.messages.push({
+                line: event.line,
+                column: event.column,
+                token: event.event,
+                msg: 'event "' + event.event + '" is not defined in interface file "' + utils.toSrcPath(result['interface'].file) + '"'
+              });
+            }
+          });
         });
       });
-    });
-
-    if (script.platform) {
-      let messages = checkGlobal(script.ast, script.platform);
-      script.messages = script.messages.concat(messages);
     }
-  }
+  });
 };
 
 
--- packages/chameleon-linter/linters/script.js
+++ packages/chameleon-linter/linters/script.js
@@ -1,6 +1,8 @@
 const parse = require('@babel/parser').parse;
 const config = require('../config/parser-config');
-const traverse = require('@babel/traverse')["default"];
+const traverse = require('@babel/traverse')['default'];
+const globalVars = require('../config/globalVars');
+const uniq = require('lodash.uniq');
 
 const Map = {
   watch: 'watcher',
@@ -8,7 +10,37 @@ const Map = {
   methods: 'method'
 };
 
-const handleProperty = function (property, propertyName, messages) {
+function checkArrowFun(path) {
+  let messages = [];
+  if (path.node) {
+    let properties = path.get('value').get('properties');
+    switch (path.node.key.name) {
+      case 'watch':
+      case 'computed':
+      case 'methods':
+        if (properties.forEach) {
+          (properties || []).forEach(property => {
+            messages = messages.concat(handleProperty(property, path.node.key.name));
+          });
+        }
+        break;
+      case 'beforeCreate':
+      case 'created':
+      case 'beforeMount':
+      case 'mounted':
+      case 'beforeDestroy':
+      case 'destroyed':
+        messages = messages.concat(handleProperty(path, path.node.key.name));
+        break;
+      default:
+        break;
+    }
+  }
+  return messages;
+}
+
+function handleProperty(property, propertyName) {
+  let messages = [];
   if (property.get('value').isArrowFunctionExpression()) {
     let node = property.get('key').node;
     let name = node.name;
@@ -19,9 +51,54 @@ const handleProperty = function (property, propertyName, messages) {
         ? (Map[propertyName] + ' "' + name + '" cannot be used as an arrow function')
         : ('lifecycle hook "' + name + '" cannot be used as an arrow function')
     });
-
   }
-};
+  return messages;
+}
+
+function getForbiddenGlobalTokens(platform = 'all') {
+  let tokenList = [];
+  platform = platform.toUpperCase();
+
+  Object.keys(globalVars).forEach(key => {
+    if (platform === 'ALL' || key !== platform) {
+      tokenList = tokenList.concat(globalVars[key]);
+    }
+  });
+
+  tokenList = uniq(tokenList);
+
+  return tokenList;
+}
+
+
+function checkGlobal(path, tokenList) {
+  let messages = [];
+  let tokenName = path.node.name;
+
+  // we only consider a MemberExpression or ExpressionStatement statement, and tested token must in our token list.
+  if (~['MemberExpression', 'ExpressionStatement'].indexOf(path.parent.type) && ~tokenList.indexOf(tokenName)) {
+    // check if this token has been defined as a variable on its upper levels.
+    let isGlobalVariable = true;
+    let nextScope = path.scope;
+    while (nextScope) {
+      if (nextScope.hasOwnBinding(tokenName)) {
+        isGlobalVariable = false;
+        break;
+      }
+      nextScope = nextScope.parent;
+    }
+    if (isGlobalVariable && path.parent.type != 'ObjectMethod' && path.parent.type != 'ClassMethod') {
+      messages.push({
+        line: path.node.loc.start.line,
+        column: path.node.loc.start.column,
+        token: tokenName,
+        msg: 'global variable: "' + tokenName + '" should not be used in this file'
+      });
+    }
+  }
+
+  return messages;
+}
 
 /**
  * 校验语法
@@ -30,9 +107,10 @@ const handleProperty = function (property, propertyName, messages) {
  * @return {Object}      语法检查结果
  */
 const checkSyntax = function (part) {
-  const messages = [];
+  let messages = [];
   const opts = config.script;
   let ast;
+  let tokenList = [];
   try {
     ast = parse(part.content, opts);
   }
@@ -44,33 +122,15 @@ const checkSyntax = function (part) {
     });
   }
   try {
+    tokenList = getForbiddenGlobalTokens(part.platformType || 'all');
     traverse(ast, {
-      enter(path) {
-        if (path.isClassProperty() && path.node) {
-          switch (path.node.key.name) {
-            case 'watch':
-            case 'computed':
-            case 'methods':
-              let properties = path.get('value').get('properties');
-              if (properties.forEach) {
-                (properties || []).forEach(property => {
-                  handleProperty(property, path.node.key.name, messages);
-                });
-              }
-              break;
-            case 'beforeCreate':
-            case 'created':
-            case 'beforeMount':
-            case 'mounted':
-            case 'beforeDestroy':
-            case 'destroyed':
-              handleProperty(path, path.node.key.name, messages);
-              break;
-            default:
-              break;
-          }
-        }
-
+      ClassProperty(path) {
+        // check arrow function: we do not allow arrow functions in life cycle hooks.
+        messages = messages.concat(checkArrowFun(path));
+      },
+      Identifier(path) {
+        // check global variables: we shall never use any platform specified global variables such as wx, global, window etc.
+        messages = messages.concat(checkGlobal(path, tokenList));
       }
     });
   }
