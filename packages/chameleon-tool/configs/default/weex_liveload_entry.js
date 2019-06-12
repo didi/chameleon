91cf812aeca61da4878bfef3dd2b148b65848a7e
yylgit <394720943@qq.com>
2019-06-12 17:12:02 +0800

    liveload

--- packages/chameleon-tool/configs/default/weex_liveload_entry.js
+++ packages/chameleon-tool/configs/default/weex_liveload_entry.js
@@ -7,30 +7,16 @@ var modal = weex.requireModule('modal')
 let socketInstance = bridge.initSocket({url: wsAddr});
 
 socketInstance.onopen(function (e) {
-  modal.toast({
-    message: 'onopen',
-    duration: 0.3
-  })
 })
 
 socketInstance.onmessage(function (e) {
-  modal.toast({
-    message: e.data,
-    duration: 0.3
-  })
   if (e.data === 'weex_refresh') {
     bridge.reload()
   }
 })
 socketInstance.onerror(function (e) {
-  modal.toast({
-    message: 'onerror',
-    duration: 0.3
-  })
+
 })
 socketInstance.onclose(function (e) {
-  modal.toast({
-    message: 'onclose',
-    duration: 0.3
-  })
+
 })
