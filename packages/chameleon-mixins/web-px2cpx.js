c55b1ca77c3432897bb95c9005ceb293927298e3
JiM-W <550116990@qq.com>
2019-07-03 17:30:33 +0800

    优化px2cpx

--- packages/chameleon-mixins/web-px2cpx.js
+++ packages/chameleon-mixins/web-px2cpx.js
@@ -1,7 +1,6 @@
 const _ = module.exports = {};
-const platform = process.env.platform;
 
-_.webPx2Cpx = function(px) {
+_.px2cpx = function(px) {
   function getViewportSize() {
     let viewportWidth;
     let viewportHeight;
@@ -20,32 +19,4 @@ _.webPx2Cpx = function(px) {
   const {viewportWidth} = getViewportSize();
   const cpx = +(750 / viewportWidth * px).toFixed(3);
   return cpx;
-}
-_.miniappPx2Cpx = function(px, platform) {
-  function getViewportSize() {
-    if (platform === 'wx') {
-      const { windowWidth } = wx.getSystemInfoSync();
-      return windowWidth;
-    }
-    if (platform === 'baidu') {
-      const { windowWidth } = swan.getSystemInfoSync();
-      return windowWidth;
-    }
-    if (platform === 'alipay') {
-      const { windowWidth } = my.getSystemInfoSync();
-      return windowWidth;
-    }
-  }
-
-  const viewportWidth = getViewportSize();
-  const cpx = +(750 / viewportWidth * px).toFixed(3);
-  return cpx;
-}
-_.px2cpx = function(px) {
-  if (platform === 'web') {
-    return _.webPx2Cpx(px);
-  }
-  if (['wx', 'baidu', 'alipay'].includes(platform)) {
-    return _.miniappPx2Cpx(px, platform);
-  }
 }
