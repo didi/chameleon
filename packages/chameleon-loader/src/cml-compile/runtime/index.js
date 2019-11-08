addfd61b2cd6bcc5ea3fedeef672b9a66c00c2f0
JiM-W <550116990@qq.com>
2019-11-08 11:52:01 +0800

    解决web端不生效问题

--- packages/chameleon-loader/src/cml-compile/runtime/index.js
+++ packages/chameleon-loader/src/cml-compile/runtime/index.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 const path = require('path');
-module.exports = {} = _;
+const _ = module.exports = {} ;
 _.getMiniAppRunTimeSnippet = function (platform, type) {
   return fs.readFileSync(path.join(__dirname, `./${type}.js`))
 }
