4d9a0e1affd9b97bb6bb9a293cdb0aa23ae8d11d
JiM-W <550116990@qq.com>
2019-12-13 14:35:37 +0800

    fix: 单元测试完善,解决qq小程序px2cpx

--- packages/chameleon-css-loader/test/parser-test/miniapp.test.js
+++ packages/chameleon-css-loader/test/parser-test/miniapp.test.js
@@ -2,11 +2,19 @@ const parseCss = require('../../parser/miniapp.js');
 const expect = require('chai').expect;
 
 let source = `body {width:100cpx;font-size:26px;/* height:200px; */}`;
+let alipayCSS = `.cls1{color:red} .cls2{width:100cpx;height:200cpx;}  .cls3:{font-size:30cpx;}`
 describe('parse/miniapp', function() {
   it('parse cssstyle px to rpx but leave comment in style alone', function() {
     let result = parseCss(source,{
       cmlType:'alipay',
       filePath:"chameleon-runtime/src/platform/alipay/style/index.css"
+    });
+    expect(/100rpx/.test(result)).to.be.ok;
+  })
+  it('parse cssstyle px to rpx but leave comment in style alone', function() {
+    let result = parseCss(alipayCSS,{
+      cmlType:'alipay',
+      filePath:"/src/page/index.cml"
     });
     expect(/100rpx/.test(result)).to.be.ok;
   })
