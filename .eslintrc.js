a796ec1b6de12af7f98f6fd554702c906121ea93
JiM-W <550116990@qq.com>
2019-12-27 11:38:01 +0800

    test: eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    root:true,//
+    root:true,
     env: {
       browser: true,
       es6: true,
     },
     extends: [
-      'plugin:vue/base',
-      'standard',//由于团队实现eslint规范初期可能需要修改的地方比较多
+      'standard',
     ],
     globals: {
       Atomics: 'readonly',
@@ -17,8 +16,6 @@ module.exports = {
       parser:'babel-eslint'
     },
     rules: {
-      'vue/comment-directive': 'off',
-      'vue/jsx-uses-vars': 'off',
       'semi':[2,'always']
     }
   }
